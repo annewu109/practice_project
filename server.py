6d0f6953ac1d88ff4f0746e4561aecc4b7a0d0ed
Anne Wu <annewu109@gmail.com>
2026-05-27 15:22:12 -0400

    task 6 done

--- server.py
+++ server.py
@@ -16,15 +16,20 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_to_analyze)
+    sentiment = response['label'].split('_')[1]
+    score = response['score']
+    return f"The given text has been identified as {sentiment} with a score of {score}."
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
+    return render_template("index.html")
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    '''
+    app.run(host="0.0.0.0", port=5000)
